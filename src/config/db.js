f86613357f4fc126556b641c85ba436a1bcbab0b
Vishal Mauriya <vishal.mauriya@digimantra.com>
2026-08-28 19:39:47 +0530

    feat: add MongoDB connection and Express bootstrap

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  const uri = process.env.MONGODB_URI;
+
+  if (!uri) {
+    throw new Error('MONGODB_URI is not set');
+  }
+
+  await mongoose.connect(uri);
+  console.log('MongoDB connected');
+};
+
+module.exports = connectDB;
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', (req, res) => {
   res.json({ message: 'Event Ticket API' });
 });
 
+router.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
 module.exports = router;
