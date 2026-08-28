e6d49ae241b8794751f069e669aa89b40d6a0b5e
Vishal Mauriya <vishal.mauriya@digimantra.com>
2026-08-29 00:26:15 +0530

    feat: add security middleware (helmet, cors, rate limiting)

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const corsOptions = require('./config/cors');
 const routes = require('./routes');
 const notFound = require('./middlewares/notFound');
 const errorMiddleware = require('./middlewares/errorMiddleware');
 
 const app = express();
+
+app.use(helmet());
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use('/api', routes);
